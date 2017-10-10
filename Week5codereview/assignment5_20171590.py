21c0cf16c0ece3b0120455bc54528365d87d7250
kookminju <sus-17@hanmail.net>
2017-10-10 10:53:47 +0900

    Update assignment5_20171590.py

--- Week5codereview/assignment5_20171590.py
+++ Week5codereview/assignment5_20171590.py
@@ -11,8 +11,8 @@ def iterfibo(n):
         return n
     else:
         a = [0,1]
-        for i in range(2, n+1):
-            a.append(a[i-2] + a[i-1])
+        for i in range(n-1):
+            a.append(a[i] + a[i+1])
         return a[n]
 
 while True:
