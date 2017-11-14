678b5e0fcf82077489759cf5ce0d81aed3a54e84
kookminju <sus-17@hanmail.net>
2017-11-14 11:00:37 +0900

    김민주

---
+++ Week11codereview/keypad.py
@@ -0,0 +1,30 @@
+import calcFunctions as calc
+
+numPadList = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0', '.', '=',
+]
+
+operatorList = [
+    '*', '/',
+    '+', '-',
+    '(', ')',
+    'C',
+]
+
+# 리스트를 딕셔너리로 바꿈
+constantDic = {
+    'pi':'3.141592',
+    '빛의 이동 속도 (m/s)':'3E+8',
+    '소리의 이동 속도 (m/s)':'340',
+    '태양과의 평균 거리 (km)':'1.5E+8',
+}
+
+functionList = [
+    'factorial (!)' ,
+    '-> binary' ,
+    'binary -> dec' ,
+    '-> roman'
+]
