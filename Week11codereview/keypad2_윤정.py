0840f0cde006ed4dcd969d1ae461becded5eb25e
wwjdtm <jje0ng@kookmin.ac.kr>
2017-11-14 10:47:18 +0900

    Add files via upload

---
+++ Week11codereview/keypad2_윤정.py
@@ -0,0 +1,27 @@
+numPadList = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0', '.', '=',
+]
+
+operatorList = [
+    '*', '/',
+    '+', '-',
+    '(', ')',
+    'C',
+]
+
+constantList = {
+    'pi':str(3.141592),
+    '빛의 이동 속도 (m/s)':str(3E+8),
+    '소리의 이동 속도 (m/s)':str(340),
+    '태양과의 평균 거리 (km)':str(1.5E+8),
+}
+
+functionList = [
+    'factorial (!)',
+    '-> binary',
+    'binary -> dec',
+    '-> roman',
+]
